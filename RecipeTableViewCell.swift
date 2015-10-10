450d319ee9acac9d5e959295f248f74e9c417832
Nicholas Cai <nicholascai@calvisitor-10-105-150-108.CalVisitor.1918.Berkeley.EDU>
2015-10-10 15:46:50 -0700

    Added table view

---
+++ RecipeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeTableViewCell.swift
+//  FastFashion
+//
+//  Created by Nicholas Cai on 10/10/15.
+//  Copyright © 2015 Nicholas Cai. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
